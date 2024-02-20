2e99b259a8811a3a4ac365e33bb6c8a198333c72
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-20 11:51:21 +0530

    created message model

---
+++ backend/db/models/message.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    //who was the sender
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId  ,
+        ref:"User" ,//senderIf will be the id from the user model that we have created i.e. User._id
+        required:true
+        },
+    //who was the reciever
+    recieverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    //what were the messsages
+    message:{
+        type:String,
+        required:true
+    }
+
+    //createdAt and updatedAt fields --> timestamps
+},{timestamps:true});
+
+const Message = mongoose.model("Message",messageSchema);
+
+
+
+export default Message;
