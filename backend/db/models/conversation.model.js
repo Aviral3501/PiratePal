a0c7a353605a517a24ce6e6d6e24a040e40fb202
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-20 11:57:17 +0530

    created conversation model whil will contain all the participants and the messages in the chat

--- backend/db/models/conversation.model.js
+++ backend/db/models/conversation.model.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    
+    participants:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",//take the user ids from the user model
+        }
+    ],
+    messages : [
+      {
+            type:mongoose.Schema.Types.ObjectId, 
+            ref:"Message",//take the messages ids from the message model
+            default:[],//conversation array will be empty by default
+      }
+    ]
 },{timestamps:true});
+
+const Conversation = mongoose.model("Conversation" , conversationSchema);
+export default Conversation;
