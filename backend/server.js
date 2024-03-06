d516975bb914be8b3a8e41456c9c62e93682d72d
Aviral Singh <aviralsingh3501@gmail.com>
2024-03-06 05:47:34 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+
 const __dirname = path.resolve();
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));//gives us absolute path to root
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
